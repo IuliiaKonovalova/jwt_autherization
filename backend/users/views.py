644414cfc2bdb0c4622134c8b2dffc99fb2d1bac
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-04 19:53:03 +0800

    Add Register view

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from .serializers import UserCreateSerializer
 
-# Create your views here.
+
+class RegisterView(APIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    def post(self, request):
+        data = request.data
+        first_name = data['first_name']
+        last_name = data['last_name']
+        email = data['email']
+        password = data['password']
+
+        user = User.objects.create_user(
+            first_name,
+            last_name,
+            email,
+            password,
+        )
+
+        user = UserCreateSerializer(user)
+
+        return Response(user.data, status=status.HTTP_201_CREATED)
+
+
+class RetrieveUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        pass
