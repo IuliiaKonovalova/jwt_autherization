c6f15526fb41caa7964ef128d23a7753780d0011
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-05 15:21:09 +0800

    Add validate method to userCreateSerializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -8,6 +10,18 @@ class UserCreateSerializer(serializers.ModelSerializer):
         model = User
         fields = ('first_name', 'last_name', 'email', 'password')
 
+    def validate(self, data):
+        user = User(**data)
+        password = data.get('password')
+        try:
+            validate_password(password, user)
+        except exceptions.ValidationError as e:
+            serializer_error = serializers.as_serializer_error(e)
+            raise exceptions.ValidationError(
+                {
+                  'password': serializer_error['non_field_errors']
+                }
+            )
     def create(self, validated_data):
         user = User.objects.create_user(
             first_name=validated_data['first_name'],
@@ -17,3 +31,9 @@ class UserCreateSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
