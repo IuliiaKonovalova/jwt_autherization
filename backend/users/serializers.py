9af9bc81a008061eb080201f9214ed4bfb342147
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-05 14:15:47 +0800

    Add create user method to serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -6,4 +6,14 @@ User = get_user_model()
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('__all__')
+        fields = ('first_name', 'last_name', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+        )
+
+        return user
