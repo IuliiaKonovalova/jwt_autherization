668860304b141f7a0632ca3b83184770517af92b
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-08 18:17:04 +0800

    Add settings for routes

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+
+require('dotenv').config();
+
+const registerRouter = require('./routes/auth/register');
+
 const app = express();
 
+app.use(express.json());
+
+app.use(registerRouter);
+
 app.use(express.static('client/build'));
+
 app.get('*', (req, res) => {
   const myPath = path.resolve(__dirname, 'client', 'build', 'index.html');
-  console.log('__dirname', __dirname);
-  console.log('myPath', myPath);
-  return res.sendFile(myPath);
 });
 
 const PORT = process.env.PORT || 5000;
