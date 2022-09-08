5dce42dfd391aefb0346b769a0b7340c001a6148
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-08 19:04:38 +0800

    Add register form to register page

--- frontend/client/src/containers/RegisterPage.js
+++ frontend/client/src/containers/RegisterPage.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import Layout from 'components/Layout';
+import { Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 const RegisterPage = () => {
+  const { register } = useSelector(state => state.user);
+
+
+
+  if (register)
+    return <Navigate to='/login' />;
+  
   return (
     <Layout title='Auth Site | Register' content='Register page'>
-      <h1>RegisterPage</h1>
+      <h1>Register for an Account</h1>
+      <form>
+        <div>
+          <label htmlFor='first_name'>First Name</label>
+          <input type='text' name='first_name' id='first_name' />
+        </div>
+        <div>
+          <label htmlFor='last_name'>Last Name</label>
+          <input type='text' name='last_name' id='last_name' />
+        </div>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email' id='email' />
+        </div>
+      </form>
     </Layout>
   )
 }
