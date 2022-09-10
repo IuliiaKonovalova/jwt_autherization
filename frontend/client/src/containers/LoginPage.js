55407e00e75b2f56846c3fded1257a70a0c5df7c
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-10 21:18:04 +0800

    Add Login form to Login page

--- frontend/client/src/containers/LoginPage.js
+++ frontend/client/src/containers/LoginPage.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { resetRegistered } from 'features/user';
 import Layout from 'components/Layout';
@@ -21,7 +21,42 @@ const LoginPage = () => {
 
   return (
     <Layout title='Auth Site | Login' content='Login page'>
-      <h1>LoginPage</h1>
+      <h1>Log into your Account</h1>
+			<form className='mt-5' onSubmit={onSubmit}>
+				<div className='form-group'>
+					<label className='form-label' htmlFor='email'>
+						Email
+					</label>
+					<input
+						className='form-control'
+						type='email'
+						name='email'
+						onChange={onChange}
+						value={email}
+						required
+					/>
+				</div>
+				<div className='form-group mt-3'>
+					<label className='form-label' htmlFor='password'>
+						Password
+					</label>
+					<input
+						className='form-control'
+						type='password'
+						name='password'
+						onChange={onChange}
+						value={password}
+						required
+					/>
+				</div>
+				{loading ? (
+					<div className='spinner-border text-primary' role='status'>
+						<span className='visually-hidden'>Loading...</span>
+					</div>
+				) : (
+					<button className='btn btn-primary mt-4'>Login</button>
+				)}
+			</form>
     </Layout>
   )
 }
