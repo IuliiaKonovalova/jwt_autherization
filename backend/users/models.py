42479fd4531c856b97d1162ec48717c19e053e6b
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-16 12:23:47 +0800

    Add explicite comments to models.py code

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,11 +7,18 @@ from django.contrib.auth.models import (
 
 
 class UserAccountManager(BaseUserManager):
+    """Manager for user accounts"""
     def create_user(self, first_name, last_name, email, password=None):
+        """Create a new user account"""
         if not email:
             raise ValueError("Users must have an email address")
 
+        # control ending of the email address
+        # EMAIL@MAIL.COM -> EMAIL@mail.com
         email=self.normalize_email(email)
+
+        # to control the email address format
+        # EMAIL@mail.com -> email@mail.com
         email = email.lower()
         user = self.model(
             first_name=first_name,
@@ -24,12 +31,14 @@ class UserAccountManager(BaseUserManager):
         return user
 
     def create_superuser(self, first_name, last_name, email, password=None):
+        """Create and save a new superuser with given details"""
         user = self.create_user(
             first_name,
             last_name,
             email,
             password=password,
         )
+        # is_staff and is_superuser
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -37,6 +46,7 @@ class UserAccountManager(BaseUserManager):
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
