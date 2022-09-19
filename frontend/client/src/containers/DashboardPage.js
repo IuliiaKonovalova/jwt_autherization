356b1b764fe34ce69d5de35bc0ed0d84686963f9
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-19 17:52:50 +0800

    Add sum up and loading logic to dashboard

--- frontend/client/src/containers/DashboardPage.js
+++ frontend/client/src/containers/DashboardPage.js
@@ -1,9 +1,25 @@
+import { userSelector } from 'react-redux';
 import Layout from 'components/Layout';
 
 const DashboardPage = () => {
   return (
     <Layout title='Auth Site | Dashboard' content='Dashboard page'>
-      <h1>Dashboard</h1>
+      {/* check the status */}
+      {loading || user === null? (
+        <div className='spinner-border text-primary' role='status'>
+          <span className='sr-only'>Loading...</span>
+          </div>
+      ) : (
+        <>
+          <h1>Dashboard</h1>
+          <p>User Details</p>
+          <ul>
+            <li>First Name: {user.first_name}</li>
+            <li>Last Name: {user.last_name}</li>
+            <li>Email: {user.email}</li>
+          </ul>
+        </>
+      )}
     </Layout>
   )
 }
