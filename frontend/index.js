859cfe495d4da7026f0ec482bb9fb65fd99a361b
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-20 14:23:18 +0800

    Add router handler and hook for verify to express

--- frontend/index.js
+++ frontend/index.js
@@ -4,11 +4,12 @@ const path = require('path');
 
 require('dotenv').config();
 
-
+// routers handlers for express
 const loginRouter = require('./routes/auth/login');
 const logoutRouter = require('./routes/auth/logout');
 const meRouter = require('./routes/auth/me');
 const registerRouter = require('./routes/auth/register');
+const verifyRouter = require('./routes/auth/verify');
 
 
 const app = express();
@@ -16,11 +17,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-
+// hook with app
 app.use(loginRouter);
 app.use(logoutRouter);
 app.use(meRouter);
 app.use(registerRouter);
+app.use(verifyRouter);
 
 app.use(express.static('client/build'));
 
